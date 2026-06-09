8ef6ba54a15ddefc09253d0baadbc6adc7017a6b
Azid-ops <mahad5063@gmail.com>
2026-06-10 02:53:04 +0500

    Added autoscaling again

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   @Get('hello') // Ye full path ban gaya: /api/hello
   getHello(): any {
     return { 
-      message: "Hello from NestJS and probing!",
+      message: "Hello from NestJS.",
       status: "success",
       timestamp: new Date().toISOString()
     };
