29c3294d3cae4a6d9dad57da66c96d903997e6dc
Azid-ops <mahad5063@gmail.com>
2026-06-10 21:58:16 +0500

    Added console

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   
   // Instance get karne ka sahi tareeqa
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('PORT') || 5000; // Default fallback bhi zaroori hai
+  const port = configService.get<string>('PORT') || 3000; // Default fallback bhi zaroori hai
+  console.log("Hello");
+  console.log(port);
   
   await app.listen(port);
   console.log(`Application is running on port: ${port}`);
